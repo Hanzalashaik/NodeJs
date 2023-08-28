4e1de8224d943a7aa50cb45ffd6b38157d838804
Hanzalashaik <shaikhhanzala27@gmail.com>
2023-08-28 14:58:17 +0000

    Github CLI

--- axioss/task.js
+++ axioss/task.js
@@ -1,33 +1,34 @@
 import axios from "axios";
-import readline from "readline-sync"
+import readline from "readline-sync";
+import cli from "cli-color"
 
+async function getData() {
+    try {
+        let c1=cli.xterm(34);
+        let c2=cli.xterm(196);
+        let c3=cli.xterm(228);
+        let c4=cli.xterm(90);
+        let c5=cli.xterm(201);
+        let c6=cli.xterm(84);
+        console.log(c5("*****************WELCOME TO GITHUB CLI**********************"));
+        
+        let username = readline.question("Enter your username:");
+        let url = `https://api.github.com/users/${username}`;
 
-function getData(){
-    return new Promise((resovle,reject)=>{
-
-        let username=readline.question("Enter your username:");
-        let url=`https://api.github.com/users/${username}`
-    
-        axios.get(url).then((response)=>{
-            resovle(response.data);
-        }).catch((error)=>{
-            reject(error);
-        })
-    })
-    
+        let output = await axios(url);
+        console.log(c3("Name:"),output.data.name);
+        console.log(c2("CompanyName:"),output.data.company);
+        console.log(c1("Location:"),output.data.location);
+        console.log(c4("Bio:"),output.data.bio);
+        console.log(c5("Twitter_Username:"),output.data.twitter_username);
+        console.log(c2("Public Repo:"),output.data.public_repos);
+        console.log(c3("Public Gist:"),output.data.public_gists); // Corrected property name
+        console.log(c4("Followers:"),output.data.followers);
+        console.log(c6("Following:"),output.data.following);
+    } catch (e) {
+        console.log("Invalid Username");
+        // console.log(e);
+    }
 }
-getData().then((data)=>{
 
-console.log(data.name);
-    console.log(data.company);
-    console.log(data.location);
-    console.log(data.bio);
-    console.log(data.twitter_username);
-    console.log(data.public_repos);
-    console.log(data.public_gists);
-    console.log(data.followers);
-    console.log(data.following);
-}).catch((error)=>{
-    console.log("Invalid github usename",error);
-    
-})
+getData();
